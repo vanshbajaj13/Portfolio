911975d21e8c55aa7c22f4f9f844d39bd57aed09
vanshbajaj13 <vanshbajaj705@gmail.com>
2023-04-28 19:59:35 +0530

    change text animation

--- frontend/src/components/Home/Name.jsx
+++ frontend/src/components/Home/Name.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 const Name = (props) => {
+  useEffect(() => {
+    const sp = document.getElementsByClassName("blast");
+    for(let i = 0 ; i < (sp).length ;i++){
+        sp[i].addEventListener("mouseover",()=>{
+            sp[i].classList.add("rubber");
+            setTimeout(() => {
+                sp[i].classList.remove("rubber");
+            }, 1000);
+        })
+    }
+}, [])
   return (
         <span className="blast">
             {props.char}
